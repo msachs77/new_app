17e41cb548f41ccde69ac8311877239da786510a
msachs77 <sachs.matthew@gmail.com>
2013-12-29 22:21:31 -0500

    added gryphons and home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jquery
-//= require jquery_ujs
-//= require bootstrap
 //= require jquery.turbolinks
 //= require turbolinks
+//= require jquery_ujs
+//= require bootstrap
 //= require masonry/jquery.masonry
-//= require_tree.
+//= require_tree .
